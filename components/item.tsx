5b160e367b89216a680a6ff08705d2fab3f1fd7b
Aaron <aafinn@utu.fi>
2025-05-21 16:36:57 +0300

    buy animation

--- components/item.tsx
+++ components/item.tsx
@@ -1,7 +1,9 @@
-import { View, Text, ScrollView, Button } from "react-native";
+import { useEffect } from "react";
+import { View, Text, ScrollView, Button, Pressable } from "react-native";
 import { ItemType } from "@/app/shop";
 import { Ionicons } from "@expo/vector-icons";
 import { useCounter } from "./CounterContext";
+import Animated, { useAnimatedStyle, withRepeat, withSpring, useSharedValue } from "react-native-reanimated";
 
 type ItemProps = {
     item: ItemType;
@@ -13,6 +15,7 @@ const Item = ({ item, itemClass }: ItemProps) => {
     const {num, setNum} = useCounter();
     const {incAct, setIncAct} = useCounter();
     const {incPas, setIncPas} = useCounter();
+    const offset = useSharedValue(0);
 
     const handlePress = () => {
         if (itemClass === "active") {
@@ -21,7 +24,18 @@ const Item = ({ item, itemClass }: ItemProps) => {
         if (itemClass === "passive") {
             setIncPas(incPas + item.incNum);
         }
+        pressAnimation();
     }
+
+    const pressAnimation = () => {
+        offset.value = 0;
+
+        offset.value = withRepeat(withSpring(20), 1, true);
+    }
+
+    const animatedStyles = useAnimatedStyle(() => ({
+        transform: [{ translateX: offset.value }],
+      }))
 
     let iconName: keyof typeof Ionicons.glyphMap = "hand-left-sharp";
     let iconSize: number = 10;
@@ -36,11 +50,19 @@ const Item = ({ item, itemClass }: ItemProps) => {
 
     return (
         <View>
-            <Text className="mt-12 mb-8">{item.name} {item.cost} {item.incNum}</Text>
+            <Text className="mt-12 mb-8 text-4xl">{item.name} {item.cost} {item.incNum}</Text>
             <View className="w-24 h-24">
                 <Ionicons name={iconName} size={iconSize} color={iconColor}/>
             </View>
-            <Button onPress={handlePress} title="" color="black"/>
+            <Animated.View style={animatedStyles}>
+                <Pressable onPress={handlePress} style={({ pressed }) => [
+                    {
+                    backgroundColor: "white"
+                    },
+                ]}>
+                    <Text> buy item </Text>
+                </Pressable>
+            </Animated.View>
         </View>
     )
 }
